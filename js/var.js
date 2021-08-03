a2cca4b0b7e34d06c1693ccf69c188792aad6de4
aabharathore0603 <69072330+aabharathore0603@users.noreply.github.com>
2021-08-03 16:17:57 +0530

    first commit

---
+++ js/var.js
@@ -0,0 +1,12 @@
+var num = '+91-12345 67890';
+var email = 'abcd@gmail.com';
+var lin = 'abcd-8102';
+
+document.getElementById('no').innerHTML = num;
+document.getElementById('em').innerHTML = email;
+document.getElementById('ln').innerHTML = lin;
+
+
+console.log(num);
+console.log(num);
+console.log(num);
